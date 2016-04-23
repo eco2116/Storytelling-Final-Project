a4941da47da433ee51b75a9b3d188d617ff752ec
Avi Chad-Friedman <avichad-friedman@MBP502.local>
2016-04-23 11:39:27 -0400

    documenting and parsing address '

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Storytelling-Final-Project
+
+1. To install the required python packages:
+	`pip install -r requirements.txt`
--- lib/alert_api.py
+++ lib/alert_api.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect
 import redis
+import time
+import usaddress
 
 import twilio.twiml
 
@@ -8,16 +10,50 @@ conn = redis.Redis()
 
 @app.route("/", methods=['GET', 'POST'])
 def process_food_event():
-
-	food_alert = {}
+	
+	t = time.time()
 	resp = twilio.twiml.Response()
 	print request.form['Body']
 	print request.form['FromCity']
 	print request.form['FromZip']
-	food_alert
 
+	#commented for testing in Boston
+	# if(request.form['FromCity'] != 'New York'):
+	# 	resp.message("Sorry, we only process events within NYC!")
+	# 	return resp
 
+	if(request.form['Body']):
+		body_info = parse_message(request.form['Body'])
+		food_alert_dict = {
+			'establishment' : body_info['establishment'],
+			'location' : body_info['location'],
+			'zip' : request.form['FromZip']
+		}	
+
+	food_alert_dict = {
+		'establishment' : request.form['Body'],
+		'zip' : request.form['FromZip']
+	}
+	conn.hmset(t, food_alert_dict)
 	return str(resp)
+
+
+# @app.route("/subscribe", methods=['GET', 'POST'])
+# def subscribe_user():
+
+def parse_message(text):
+	"""
+	Parses an alert text in the form <establishment name>, <address of any format>
+    Parameters:
+    	text - the body of the twilio request
+    Returns:
+    	dict - dictionary with name of establishment and parsed address dictionary
+
+    """
+	components = string.split(',')
+	return {establishment : components[0], location : usaddress.parse(components[1])}
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
