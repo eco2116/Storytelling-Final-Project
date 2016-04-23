ac9ac0a3a1c202447f2834efc9238e16df202353
ckspyy <yao.yang.cksp@gmail.com>
2016-04-23 18:55:33 -0400

    Update shelterstatusV1.py

--- shelterstatusV1.py
+++ shelterstatusV1.py
@@ -1,14 +1,25 @@
+
+# this python code retrieves target tweets on NYC shelter information and process the data to be stored in a Amazon AWS RDS database
+# the data in the database will be later used for visualiztion on the website to provide homeless people latest shelter statuses in NYC
+
+
+
+# import necessary modules
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy import API
 from tweepy import AppAuthHandler
 from tweepy import Cursor
 from pymysql import Error
+# database module
 import pymysql
+# data processing module
 import re
 import time
 import datetime
 
+
+# insert the tweet info into database
 def insert(cur,data):
     """Insert a record"""
     sql = "INSERT INTO Shelter VALUES ('%s','%s','%s','%s','%s','%s','%s','%s')" %(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7])
@@ -19,6 +30,7 @@ def insert(cur,data):
     except pymysql.Error as e:
         print(e)
 
+# process tweets into a uniform format for later usage on visualization
 def process_tweet(text):
     for (pattern, repl) in patterns: 
         text = re.sub(pattern, repl, text)
@@ -31,15 +43,19 @@ csecret=""
 atoken=""
 asecret=""
 
+# authentification
 auth = AppAuthHandler(ckey, csecret)
 api = API(auth)
 
+# establish connection to database
 conn = pymysql.connect(db='bigdata', host='', port=3306,user='',passwd='',use_unicode=True, charset='utf8')
 cur = conn.cursor()
 
+# rules for process the original tweets
 replacement_patterns = [ (r'@\S*',''),(r'#\S*',''),(r'rt\S*','')]
 patterns = [(re.compile(pattern), repl) for (pattern, repl) in replacement_patterns]
 
+# get the last update time and start searching after it
 sql = 'select max(date) from Shelter'
 cur.execute(sql)
 lasttime=cur.fetchone()[0]
@@ -48,6 +64,8 @@ if lasttime==None:
 searchterms=['ckspyy']
 
 print lasttime
+
+# find all latest tweets and put processed info into database
 for eachsearch in searchterms:
     print(eachsearch)
     for tweets in Cursor(api.search, q=eachsearch,lang="en",count=100).pages(30):
@@ -66,6 +84,7 @@ for eachsearch in searchterms:
             except Error as e:
                 print(e)
 
+# close connection
 cur.close()
 conn.close()
 
