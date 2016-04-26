e821fa9f1174c0b36f44a16931c6f26a757d3621
Evan O'Connor <eco2116@columbia.edu>
2016-04-26 02:14:49 -0400

    initial positive and negative articles

--- nytimes/bin/nyt_homeless_api.py
+++ nytimes/bin/nyt_homeless_api.py
@@ -21,7 +21,42 @@ def get_moving_avg():
 		avg = conn.get('movingAvgArticleSubjectivity')
 	return jsonify({'moving_avg' : avg})
 
+@app.route("/positive_article", methods=['GET'])
+def get_positive_article():
+	max_val = -1
+	positive_article = ""
+	keys = conn.keys()
+	for key in keys:
+		try:
+			pos = conn.hget(key, 'polarity')
+			if pos > max_val:
+				max_val = pos
+				positive_article = conn.hget(key, 'title')
+
+		except Exception:
+			continue
+	print max_val
+	return positive_article
+
+@app.route("/negative_article", methods=['GET'])
+def get_positive_article():
+	min_val = 1
+	negative_article = ""
+	keys = conn.keys()
+	for key in keys:
+		try:
+			pos = conn.hget(key, 'polarity')
+			if pos < min_val:
+				min_val = pos
+				negative_article = conn.hget(key, 'title')
+
+		except Exception:
+			continue
+	print min_val
+	return negative_article
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
+
+
